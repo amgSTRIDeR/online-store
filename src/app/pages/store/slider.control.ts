b956e7d977bb9c8c27778872db1b82df17caca3e
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-01-10 15:07:16 +0400

    feat: add slider scrolls

--- src/app/pages/store/slider.control.ts
+++ src/app/pages/store/slider.control.ts
@@ -1,52 +1,81 @@
-
 export class StoreSlider {
-  static direction = 'horizontal';
-  static currentPageNumber = 1;
+    static direction = 'horizontal';
+    static currentPageNumber = 1;
 
-  static sliderRender() {
-    const cardsWrapper = document.querySelector('.cards');
-    // const cardArray = Array.from(document.querySelectorAll('.card'));
-    // const scrollWrapper = document.querySelector('.scroll');
-    // const scrollsArray = Array.from(document.querySelectorAll('.scroll__item'));
-    const arrowLeft = document.querySelector('.arrow-left');
-    const arrowRight = document.querySelector('.arrow-right');
+    static sliderRender() {
+        const cardsWrapper = document.querySelector('.cards');
+        // const cardArray = Array.from(document.querySelectorAll('.card'));
 
-    if (cardsWrapper instanceof HTMLDivElement) {
-      cardsWrapper.style.transform = `translateX(${(this.currentPageNumber - 1) * -72}vw)`
+        const arrowLeft = document.querySelector('.arrow-left');
+        const arrowRight = document.querySelector('.arrow-right');
+
+        if (cardsWrapper instanceof HTMLDivElement) {
+            cardsWrapper.style.transform = `translateX(${
+                (StoreSlider.currentPageNumber - 1) * -72
+            }vw)`;
+        }
+
+        StoreSlider.scrollsRender();
+
+        arrowLeft?.addEventListener('click', () => {
+            if (StoreSlider.currentPageNumber > 1) {
+                StoreSlider.currentPageNumber -= 1;
+                StoreSlider.moveSlider();
+            }
+        });
+
+        arrowRight?.addEventListener('click', () => {
+            if (StoreSlider.currentPageNumber < StoreSlider.pageCounter()) {
+                StoreSlider.currentPageNumber += 1;
+                StoreSlider.moveSlider();
+            }
+        });
     }
 
-    arrowLeft?.addEventListener('click', () => {
-      if (this.currentPageNumber > 1) {
+    static pageCounter(numberOfProductsPerPage = 6) {
+        const cardArray = Array.from(document.querySelectorAll('.card'));
+        let visibleProductsCounter = 0;
+
+        cardArray.forEach((e) => {
+            if (e instanceof HTMLDivElement) {
+                if (e.style.display === 'grid') {
+                    visibleProductsCounter += 1;
+                }
+            }
+        });
+        return Math.ceil(visibleProductsCounter / numberOfProductsPerPage);
+    }
+
+    static scrollsRender() {
+        const scrollWrapper = document.querySelector('.scroll');
+        if (scrollWrapper instanceof HTMLUListElement) {
+            scrollWrapper.innerHTML = '';
+
+            for (let i = 0; i < StoreSlider.pageCounter(); i += 1) {
+                const scrollItem = document.createElement('li');
+                scrollItem.classList.add('scroll__item');
+
+                if (i === StoreSlider.currentPageNumber - 1) {
+                    scrollItem.classList.add('scroll__item_active');
+                }
+
+                const copyCard = scrollItem.cloneNode(true);
+                copyCard.addEventListener('click', () => {
+                    StoreSlider.currentPageNumber = i + 1;
+                    StoreSlider.moveSlider();
+                });
+                scrollWrapper.appendChild(copyCard);
+            }
+        }
+    }
+
+    static moveSlider() {
+        const cardsWrapper = document.querySelector('.cards');
         if (cardsWrapper instanceof HTMLDivElement) {
-          this.currentPageNumber -= 1;
-          cardsWrapper.style.transform = `translateX(${(this.currentPageNumber - 1) * -72}vw)`
+            cardsWrapper.style.transform = `translateX(${
+                (StoreSlider.currentPageNumber - 1) * -72
+            }vw)`;
+            StoreSlider.scrollsRender();
         }
-      }
-    })
-
-    arrowRight?.addEventListener('click', () => {
-      if (this.currentPageNumber < this.pageCounter()) {
-        if (cardsWrapper instanceof HTMLDivElement) {
-          this.currentPageNumber += 1;
-          cardsWrapper.style.transform = `translateX(${(this.currentPageNumber - 1) * -72}vw)`
-        }
-      }
-    })
-  }
-
-  static pageCounter(numberOfProductsPerPage = 6) {
-    const cardArray = Array.from(document.querySelectorAll('.card'));
-    let visibleProductsCounter = 0;
-
-    cardArray.forEach((e) => {
-      if (e instanceof HTMLDivElement) {
-        if (e.style.display === 'grid') {
-          visibleProductsCounter += 1;
-        }
-      }
-    });
-    return Math.ceil(visibleProductsCounter / numberOfProductsPerPage);
-  }
-
-
+    }
 }
