c92785ef6eccea5d7925ce7e625e64129ef38446
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-01-12 14:35:11 +0400

    feat: cards view direction depends on query parameters

--- src/app/shared/functions/storePageListener.ts
+++ src/app/shared/functions/storePageListener.ts
@@ -1,28 +1,11 @@
-import { StoreSlider } from "../../pages/store/slider.control";
+import { changeCardsDirection } from "./cahge-cards-direction";
 
 export function addStorePageListener() {
   const goodsView = document.querySelector('.goods-view');
-  const store = document.querySelector('.store');
 
   if (goodsView instanceof HTMLSelectElement) {
     goodsView.addEventListener('change', () => {
-      if(store) {
-        if (goodsView.value === '0') {
-          store.classList.remove('store_horizontal');
-          store.classList.remove('store_vertical');
-          store.classList.add('store_horizontal');
-          StoreSlider.direction = 0;
-          StoreSlider.renewSlider();
-          StoreSlider.setQuery();
-        } else {
-          store.classList.remove('store_horizontal');
-          store.classList.remove('store_vertical');
-          store.classList.add('store_vertical');
-          StoreSlider.direction = 1;
-          StoreSlider.renewSlider();
-          StoreSlider.setQuery();
-        }
-      }
+      changeCardsDirection(+goodsView.value);
     })
   }
 }
