1db504aeadc62f9be52183096834383873f20cb3
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-01-15 20:05:38 +0400

    refactor: modal window

---
+++ src/app/pages/modal-window/address-input-check.ts
@@ -0,0 +1,19 @@
+import { ModalWindow } from "./modalWindow";
+
+export function addressInputCheck(inputElement: HTMLInputElement) {
+  const inputText = inputElement.value;
+  ModalWindow.addressCheck = false;
+
+  const addressExp = /^([а-яА-Яa-zA-Z0-9]{5,})( [а-яА-Яa-zA-Z0-9]{5,}){2,}$/;
+  if (inputText.match(addressExp)) {
+      ModalWindow.addressCheck = true;
+  }
+
+  if (ModalWindow.addressCheck === true) {
+      inputElement.classList.add('input_true');
+      inputElement.classList.remove('input_false');
+  } else {
+      inputElement.classList.add('input_false');
+      inputElement.classList.remove('input_true');
+  }
+}
